ecd8d5949c2f54b61ad9222d4dc8c1148a148eb7
Jon Bonso <jsbonso@gmail.com>
2016-11-18 09:00:40 +1100

    Added Stock Class

---
+++ src/app/stock.ts
@@ -0,0 +1,8 @@
+export class Stock {
+
+    constructor( public id: number, public stockName: string, 
+                 public stockCode: string, public stockPrice: number, public stockMarket?: string) {
+
+    }
+
+}
