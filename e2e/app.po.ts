68a4290f721d36c9906247cf48c1b901c5d0c9b4
Jon Bonso <jsbonso@gmail.com>
2016-11-18 08:39:16 +1100

    First Commit

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class Angular2formPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
